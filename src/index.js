cb53fc2e2232941dfc039b6d8f76be792170e753
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-10-14 19:06:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/bergen-gid">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
